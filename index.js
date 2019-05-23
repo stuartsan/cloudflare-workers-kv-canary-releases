af63be98500eae6629f178f1d8ae447a1289f2fb
Stuart Sandine <stuajc@gmail.com>
2019-05-23 15:27:41 -0700

    simplest possible case -- no stage

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ const stages = [
   }
 ];
 
-async function getRelease(request) {
+async function getDeployId(request) {
   const stateJSON = await RELEASE_STATE.get("state");
   const state = JSON.parse(stateJSON);
 
-  if (!state.nextRelease) {
-    return state.currentRelease;
+  if (!state.next) {
+    return state.current;
   }
 
   const currentStageIndex = stages.findIndex(
-    stage => stage.name === state.currentStage
+    stage => stage.name === state.stage
   );
 
   if (
@@ -42,17 +42,17 @@ async function getRelease(request) {
       .slice(0, currentStageIndex + 1)
       .some(stage => stage.criteria(request))
   ) {
-    return state.nextRelease;
+    return state.next;
   }
 
-  return state.currentRelease;
+  return state.current;
 }
 
 async function handleRequest(request) {
-  // const release = await getRelease(request);
+  const deployId = await getDeployId(request);
   const originalPath = new URL(request.url).pathname.slice(1);
   const path = originalPath === '' ? 'index.html' : originalPath;
-  const body = await APP_DEPLOYS.get(`current/${path}`);
+  const body = await APP_DEPLOYS.get(`${deployId}/${path}`);
 
   const extensionsToContentTypes = {
     'css': 'text/css',
---
+++ release.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log"
+
+	"github.com/cloudflare/cloudflare-go"
+)
+
+type ReleaseState struct {
+	Current *string `json:"current"`
+	Next    *string `json:"next"`
+	Stage   *string `json:"stage"`
+}
+
+func main() {
+	deployId := flag.String("deploy-id", "", "ID for deployment (key prefix). Why not use a git SHA?")
+	stage := flag.String("stage", "", "Release stage for deploy-id")
+	cfAPIKey := flag.String("cf-api-key", "", "CloudFlare API key")
+	cfEmail := flag.String("cf-email", "", "CloudFlare email")
+	cfAccount := flag.String("cf-account", "", "CloudFlare account id")
+	cfNamespace := flag.String("cf-kv-namespace", "RELEASE_STATE", "CloudFlare workers KV namespace name storing the release state")
+
+	flag.Parse()
+
+	if *deployId == "" {
+		log.Fatal("-deploy-id required")
+	}
+
+	api, err := cloudflare.New(*cfAPIKey, *cfEmail, cloudflare.UsingOrganization(*cfAccount))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := api.ListWorkersKVNamespaces(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var nsId string
+
+	for _, ns := range resp.Result {
+		if ns.Title == *cfNamespace {
+			nsId = ns.ID
+			break
+		}
+	}
+
+	// this is NOT a staged release, just ship it!
+	var state *ReleaseState
+
+	if *stage == "" {
+		state = &ReleaseState{
+			Current: deployId,
+			Next:    nil,
+			Stage:   nil,
+		}
+	}
+
+	releaseState, err := json.Marshal(state)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = api.WriteWorkersKV(context.Background(), nsId, "state", []byte(string(releaseState)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
